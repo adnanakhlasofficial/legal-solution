3333f05f7ebf54e04e05e4a85b73930fdf4aae74
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2024-09-15 00:05:21 +0600

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#B68C5A",
+        "light-gray": "#E7E7E7",
+        "medium-gray": "#707070",
+        "deep-gray": "#414040",
+        "light-black": "#282A36",
+        "light-white": "#F3F3F3",
+        "deep-white": "#CFCFCF",
+      },
+      fontFamily: {
+        poppins: "Poppins",
+        garamond: "EB Garamond",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
